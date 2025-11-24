2a9316629921ea1d26f719490a62da27b7048cf9
Nefarious46 <146811860+Nefarious46@users.noreply.github.com>
2025-11-24 11:35:16 +0000

    Deploying to gh-pages from @ Nefarious46/cfe_18@00c9d9615b433d0d908e27b7e2dcef7dfaacc386 🚀

---
+++ docs/authorization_logging_main/doxygen/search/classes_5.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['granularity_0',['Granularity',['../classcom_1_1teragrep_1_1cfe18_1_1requestfilter_1_1Granularity.html',1,'com::teragrep::cfe18::requestfilter']]],
+  ['group_5ftype_1',['group_type',['../enumcom_1_1teragrep_1_1cfe18_1_1handlers_1_1entities_1_1CaptureGroup_1_1group__type.html',1,'com.teragrep.cfe18.handlers.entities.CaptureGroup.group_type'],['../enumcom_1_1teragrep_1_1cfe18_1_1handlers_1_1entities_1_1HostGroup_1_1group__type.html',1,'com.teragrep.cfe18.handlers.entities.HostGroup.group_type'],['../enumcom_1_1teragrep_1_1cfe18_1_1handlers_1_1entities_1_1Linkage_1_1group__type.html',1,'com.teragrep.cfe18.handlers.entities.Linkage.group_type']]],
+  ['gxgcontrollertest_2',['GXGControllerTest',['../classcom_1_1teragrep_1_1cfe18_1_1controllerTests_1_1GXGControllerTest.html',1,'com::teragrep::cfe18::controllerTests']]]
+];
