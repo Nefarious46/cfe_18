35d5b17b30882b29272cef09abedf14af3d049dc
Nefarious46 <146811860+Nefarious46@users.noreply.github.com>
2025-12-03 15:52:18 +0000

    Deploying to gh-pages from @ Nefarious46/cfe_18@e6f59fa981b63633e6e15d74dd0a2daab8437170 🚀

---
+++ docs/EnumCapitalization/doxygen/search/all_b.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['name_0',['name',['../classcom_1_1teragrep_1_1cfe18_1_1handlers_1_1entities_1_1Cfe04Transform.html#a111cef80a1dd51f79a07ba888344051d',1,'com.teragrep.cfe18.handlers.entities.Cfe04Transform.name'],['../classcom_1_1teragrep_1_1cfe18_1_1handlers_1_1entities_1_1FileProcessing.html#aaff7c8dbea0108d874c7fd65a8cc2c9f',1,'com.teragrep.cfe18.handlers.entities.FileProcessing.name'],['../classcom_1_1teragrep_1_1cfe18_1_1handlers_1_1entities_1_1Flow.html#abf37132d5dfaab53de3fdc80a0e532ec',1,'com.teragrep.cfe18.handlers.entities.Flow.name']]],
+  ['newcapturemeta_1',['newCaptureMeta',['../classcom_1_1teragrep_1_1cfe18_1_1handlers_1_1CaptureMetaController.html#ab3cdeec0af1c71522eb64c1940c286f4',1,'com::teragrep::cfe18::handlers::CaptureMetaController']]],
+  ['newhostgroup_2',['newHostGroup',['../classcom_1_1teragrep_1_1cfe18_1_1handlers_1_1HostGroupController.html#ac934d67793487d34c6c36ec9b1cf0c58',1,'com::teragrep::cfe18::handlers::HostGroupController']]],
+  ['newline_3',['NEWLINE',['../enumcom_1_1teragrep_1_1cfe18_1_1handlers_1_1entities_1_1FileProcessing_1_1InputType.html#a95b7ee0798d93933e0462ebc73a570f1',1,'com::teragrep::cfe18::handlers::entities::FileProcessing::InputType']]],
+  ['newlinkage_4',['newLinkage',['../classcom_1_1teragrep_1_1cfe18_1_1handlers_1_1LinkageController.html#a4d65bcf2e5cff9870e60c8378a7f21fc',1,'com::teragrep::cfe18::handlers::LinkageController']]]
+];
---
+++ docs/EnumCapitalization/doxygen/search/classes_5.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['granularity_0',['Granularity',['../classcom_1_1teragrep_1_1cfe18_1_1requestfilter_1_1Granularity.html',1,'com::teragrep::cfe18::requestfilter']]],
+  ['grouptype_1',['grouptype',['../enumcom_1_1teragrep_1_1cfe18_1_1handlers_1_1entities_1_1CaptureGroup_1_1GroupType.html',1,'com.teragrep.cfe18.handlers.entities.CaptureGroup.GroupType'],['../enumcom_1_1teragrep_1_1cfe18_1_1handlers_1_1entities_1_1HostGroup_1_1GroupType.html',1,'com.teragrep.cfe18.handlers.entities.HostGroup.GroupType'],['../enumcom_1_1teragrep_1_1cfe18_1_1handlers_1_1entities_1_1Linkage_1_1GroupType.html',1,'com.teragrep.cfe18.handlers.entities.Linkage.GroupType']]],
+  ['gxgcontrollertest_2',['GXGControllerTest',['../classcom_1_1teragrep_1_1cfe18_1_1controllerTests_1_1GXGControllerTest.html',1,'com::teragrep::cfe18::controllerTests']]]
+];
