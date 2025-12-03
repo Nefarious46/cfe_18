4f9c513aa887d846eee177802fc7d2624dec99c9
Nefarious46 <146811860+Nefarious46@users.noreply.github.com>
2025-12-03 15:52:36 +0000

    Deploying to gh-pages from @ Nefarious46/cfe_18@e6f59fa981b63633e6e15d74dd0a2daab8437170 🚀

--- branches.js
+++ branches.js
@@ -1,1 +1,1 @@
-var branches = ["CaptureGroupEndpointRefactor", "doxygen_docs", "authorization_logging_main", "CaptureGroupEndpointRefactorV2", "main"]
+var branches = ["CaptureGroupEndpointRefactor", "EnumCapitalization", "doxygen_docs", "authorization_logging_main", "CaptureGroupEndpointRefactorV2", "main"]
