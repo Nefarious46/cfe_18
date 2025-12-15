9046b6c4367e1ba9ceb522ccfa7f54f39325e554
Nefarious46 <146811860+Nefarious46@users.noreply.github.com>
2025-12-15 10:25:51 +0000

    Deploying to gh-pages from @ Nefarious46/cfe_18@dad6c98246431b96486b91d5b3083dcb2fc1c2a8 🚀

--- docs/EnumCapitalisation/doxygen/search/classes_5.js
+++ docs/EnumCapitalisation/doxygen/search/classes_5.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['granularity_0',['Granularity',['../classcom_1_1teragrep_1_1cfe18_1_1requestfilter_1_1Granularity.html',1,'com::teragrep::cfe18::requestfilter']]],
-  ['grouptype_1',['grouptype',['../enumcom_1_1teragrep_1_1cfe18_1_1handlers_1_1entities_1_1CaptureGroups_1_1GroupType.html',1,'com.teragrep.cfe18.handlers.entities.CaptureGroups.GroupType'],['../enumcom_1_1teragrep_1_1cfe18_1_1handlers_1_1entities_1_1HostGroup_1_1GroupType.html',1,'com.teragrep.cfe18.handlers.entities.HostGroup.GroupType'],['../enumcom_1_1teragrep_1_1cfe18_1_1handlers_1_1entities_1_1Linkage_1_1GroupType.html',1,'com.teragrep.cfe18.handlers.entities.Linkage.GroupType']]],
+  ['grouptype_1',['GroupType',['../enumcom_1_1teragrep_1_1cfe18_1_1handlers_1_1entities_1_1Linkage_1_1GroupType.html',1,'com::teragrep::cfe18::handlers::entities::Linkage']]],
   ['gxgcontrollertest_2',['GXGControllerTest',['../classcom_1_1teragrep_1_1cfe18_1_1controllerTests_1_1GXGControllerTest.html',1,'com::teragrep::cfe18::controllerTests']]]
 ];
--- docs/EnumCapitalisation/doxygen/search/files_7.js
+++ docs/EnumCapitalisation/doxygen/search/files_7.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['interfacetype_2ejava_0',['InterfaceType.java',['../InterfaceType_8java.html',1,'']]],
-  ['ipaddress_2ejava_1',['IPAddress.java',['../IPAddress_8java.html',1,'']]]
+  ['integrationtype_2ejava_0',['IntegrationType.java',['../IntegrationType_8java.html',1,'']]],
+  ['interfacetype_2ejava_1',['InterfaceType.java',['../InterfaceType_8java.html',1,'']]],
+  ['ipaddress_2ejava_2',['IPAddress.java',['../IPAddress_8java.html',1,'']]]
 ];
