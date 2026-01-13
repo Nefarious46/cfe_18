16e491b74456c4e004cfcac93b7f062fa782fe57
Nefarious46 <146811860+Nefarious46@users.noreply.github.com>
2026-01-13 15:10:55 +0000

    Deploying to gh-pages from @ Nefarious46/cfe_18@60c7c846e15f6fdcd6a76f56c6c6cee2b71d8a54 🚀

---
+++ docs/CaptureToGroupLinkageEndpointRefactor/doxygen/search/variables_4.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['granularity_0',['granularity',['../classcom_1_1teragrep_1_1cfe18_1_1requestfilter_1_1Granularity.html#a9487bb630f69de6f7f1106fd352c9aa0',1,'com::teragrep::cfe18::requestfilter::Granularity']]],
+  ['groupid_1',['groupId',['../classcom_1_1teragrep_1_1cfe18_1_1handlers_1_1entities_1_1CaptureGroup.html#a53321a747af4357a84a887cb60f5dd5a',1,'com::teragrep::cfe18::handlers::entities::CaptureGroup']]]
+];
