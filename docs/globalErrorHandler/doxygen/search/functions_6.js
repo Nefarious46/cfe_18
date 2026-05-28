998e7f8362f86cf6c9818fd7d7d9fd98074e49de
Nefarious46 <146811860+Nefarious46@users.noreply.github.com>
2026-05-28 14:31:30 +0000

    Deploying to gh-pages from @ Nefarious46/cfe_18@dc19354ae2232e1e0b6d25fd7800de286b2ac039 🚀

---
+++ docs/globalErrorHandler/doxygen/search/functions_6.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['handlesqlexception_0',['handleSQLException',['../classcom_1_1teragrep_1_1cfe18_1_1DatabaseExceptionHandler.html#aeb8e8d0a519f359802a9e9e41d99bb7f',1,'com::teragrep::cfe18::DatabaseExceptionHandler']]]
+];
