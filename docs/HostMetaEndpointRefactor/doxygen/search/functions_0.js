0d95d4c617465ba26f895b595227348e41bd087b
Nefarious46 <146811860+Nefarious46@users.noreply.github.com>
2026-06-08 08:47:48 +0000

    Deploying to gh-pages from @ Nefarious46/cfe_18@ac99c26884ab689b2db9a3d28632509975328454 🚀

--- docs/HostMetaEndpointRefactor/doxygen/search/functions_0.js
+++ docs/HostMetaEndpointRefactor/doxygen/search/functions_0.js
@@ -3,13 +3,12 @@ var searchData=
   ['action_0',['Action',['../classcom_1_1teragrep_1_1cfe18_1_1requestfilter_1_1Action.html#a79ff4c89c16e458db305f368824396d7',1,'com::teragrep::cfe18::requestfilter::Action']]],
   ['addhub_1',['addHub',['../interfacecom_1_1teragrep_1_1cfe18_1_1HubMapper.html#a2376bffcc4aa342c88c00dd74205dd9a',1,'com::teragrep::cfe18::HubMapper']]],
   ['addlinkage_2',['addLinkage',['../interfacecom_1_1teragrep_1_1cfe18_1_1LinkageMapper.html#ad0c31ec4ea1f6e682162556f903b6614',1,'com::teragrep::cfe18::LinkageMapper']]],
-  ['addnewcapturemeta_3',['addNewCaptureMeta',['../interfacecom_1_1teragrep_1_1cfe18_1_1CaptureMetaMapper.html#acac351b0ebc56a10dfa48bda455c3762',1,'com::teragrep::cfe18::CaptureMetaMapper']]],
-  ['addnewcfe04transform_4',['addnewcfe04transform',['../interfacecom_1_1teragrep_1_1cfe18_1_1Cfe04TransformMapper.html#accf9637d65ad946234e56b1c74876f33',1,'com.teragrep.cfe18.Cfe04TransformMapper.addNewCfe04Transform()'],['../classcom_1_1teragrep_1_1cfe18_1_1handlers_1_1Cfe04TransformController.html#ac931398f7447d902c3ab233b014274e7',1,'com.teragrep.cfe18.handlers.Cfe04TransformController.addNewCfe04Transform()']]],
-  ['addnewhostgroup_5',['addNewHostGroup',['../interfacecom_1_1teragrep_1_1cfe18_1_1HostGroupMapper.html#ad2b57d9feaefadb32b3f638ea40e3e54',1,'com::teragrep::cfe18::HostGroupMapper']]],
-  ['addnewhub_6',['addNewHub',['../classcom_1_1teragrep_1_1cfe18_1_1handlers_1_1HubController.html#a4e12c721b609277248a645210f4f9c32',1,'com::teragrep::cfe18::handlers::HubController']]],
-  ['addnewstorage_7',['addNewStorage',['../classcom_1_1teragrep_1_1cfe18_1_1handlers_1_1StorageController.html#a0fab2595014bf919ab5a365fdaae3c61',1,'com::teragrep::cfe18::handlers::StorageController']]],
-  ['addstorageforcapture_8',['addStorageForCapture',['../interfacecom_1_1teragrep_1_1cfe18_1_1StorageMapper.html#a7dbf9429c3e2a1444603258c6844232d',1,'com::teragrep::cfe18::StorageMapper']]],
-  ['addstorageforflow_9',['addStorageForFlow',['../interfacecom_1_1teragrep_1_1cfe18_1_1StorageMapper.html#aef8a4ad90874676bc1880127d8c380f6',1,'com::teragrep::cfe18::StorageMapper']]],
-  ['addviewcontrollers_10',['addViewControllers',['../classcom_1_1teragrep_1_1cfe18_1_1WebAppConfig.html#a1f6648b09bc375618a616831e66caa08',1,'com::teragrep::cfe18::WebAppConfig']]],
-  ['afterall_11',['afterAll',['../classcom_1_1teragrep_1_1cfe18_1_1controllerTests_1_1MigrateDatabaseExtension.html#a2a8a5238bcb65b5c408fd9463a06ace8',1,'com::teragrep::cfe18::controllerTests::MigrateDatabaseExtension']]]
+  ['addnewcfe04transform_3',['addnewcfe04transform',['../interfacecom_1_1teragrep_1_1cfe18_1_1Cfe04TransformMapper.html#accf9637d65ad946234e56b1c74876f33',1,'com.teragrep.cfe18.Cfe04TransformMapper.addNewCfe04Transform()'],['../classcom_1_1teragrep_1_1cfe18_1_1handlers_1_1Cfe04TransformController.html#ac931398f7447d902c3ab233b014274e7',1,'com.teragrep.cfe18.handlers.Cfe04TransformController.addNewCfe04Transform()']]],
+  ['addnewhostgroup_4',['addNewHostGroup',['../interfacecom_1_1teragrep_1_1cfe18_1_1HostGroupMapper.html#ad2b57d9feaefadb32b3f638ea40e3e54',1,'com::teragrep::cfe18::HostGroupMapper']]],
+  ['addnewhub_5',['addNewHub',['../classcom_1_1teragrep_1_1cfe18_1_1handlers_1_1HubController.html#a4e12c721b609277248a645210f4f9c32',1,'com::teragrep::cfe18::handlers::HubController']]],
+  ['addnewstorage_6',['addNewStorage',['../classcom_1_1teragrep_1_1cfe18_1_1handlers_1_1StorageController.html#a0fab2595014bf919ab5a365fdaae3c61',1,'com::teragrep::cfe18::handlers::StorageController']]],
+  ['addstorageforcapture_7',['addStorageForCapture',['../interfacecom_1_1teragrep_1_1cfe18_1_1StorageMapper.html#a7dbf9429c3e2a1444603258c6844232d',1,'com::teragrep::cfe18::StorageMapper']]],
+  ['addstorageforflow_8',['addStorageForFlow',['../interfacecom_1_1teragrep_1_1cfe18_1_1StorageMapper.html#aef8a4ad90874676bc1880127d8c380f6',1,'com::teragrep::cfe18::StorageMapper']]],
+  ['addviewcontrollers_9',['addViewControllers',['../classcom_1_1teragrep_1_1cfe18_1_1WebAppConfig.html#a1f6648b09bc375618a616831e66caa08',1,'com::teragrep::cfe18::WebAppConfig']]],
+  ['afterall_10',['afterAll',['../classcom_1_1teragrep_1_1cfe18_1_1controllerTests_1_1MigrateDatabaseExtension.html#a2a8a5238bcb65b5c408fd9463a06ace8',1,'com::teragrep::cfe18::controllerTests::MigrateDatabaseExtension']]]
 ];
--- docs/HostMetaEndpointRefactor/doxygen/search/functions_c.js
+++ docs/HostMetaEndpointRefactor/doxygen/search/functions_c.js
@@ -1,15 +1,14 @@
 var searchData=
 [
   ['remoteinfo_0',['RemoteInfo',['../classcom_1_1teragrep_1_1cfe18_1_1requestfilter_1_1RemoteInfo.html#ae7aeb1fa77c5a30e1cb5e9deaa219402',1,'com::teragrep::cfe18::requestfilter::RemoteInfo']]],
-  ['removecapturemeta_1',['removeCaptureMeta',['../classcom_1_1teragrep_1_1cfe18_1_1handlers_1_1CaptureMetaController.html#afddff341135651c477e1b1155385e6ab',1,'com::teragrep::cfe18::handlers::CaptureMetaController']]],
-  ['removecapturestorage_2',['removeCaptureStorage',['../classcom_1_1teragrep_1_1cfe18_1_1handlers_1_1StorageController.html#a16c5cc858c7f76e8745a46484635e4a9',1,'com::teragrep::cfe18::handlers::StorageController']]],
-  ['removeflowstorage_3',['removeFlowStorage',['../classcom_1_1teragrep_1_1cfe18_1_1handlers_1_1StorageController.html#a08a54211dea97ca9979ee51302f597ce',1,'com::teragrep::cfe18::handlers::StorageController']]],
-  ['removehost_4',['removeHost',['../classcom_1_1teragrep_1_1cfe18_1_1handlers_1_1HostGroupController.html#a6c781998a5ad4fcc7afe4fbe40006999',1,'com::teragrep::cfe18::handlers::HostGroupController']]],
-  ['removehub_5',['removeHub',['../classcom_1_1teragrep_1_1cfe18_1_1handlers_1_1HubController.html#ad7c8f108be903c0874b6b6b16c85ca8d',1,'com::teragrep::cfe18::handlers::HubController']]],
-  ['removelinkage_6',['removeLinkage',['../classcom_1_1teragrep_1_1cfe18_1_1handlers_1_1LinkageController.html#aa043ab13e50d1e08c1f0eef11f404c82',1,'com::teragrep::cfe18::handlers::LinkageController']]],
-  ['requestidentity_7',['requestidentity',['../classcom_1_1teragrep_1_1cfe18_1_1requestfilter_1_1RequestIdentity.html#ac9dbf431f090b215ae3db0ee9f00b027',1,'com.teragrep.cfe18.requestfilter.RequestIdentity.RequestIdentity()'],['../classcom_1_1teragrep_1_1cfe18_1_1requestfilter_1_1RequestIdentity.html#a0cba0e4d72a6e7fcd1bf02f58d7d15ba',1,'com.teragrep.cfe18.requestfilter.RequestIdentity.RequestIdentity(final UUID uuid)']]],
-  ['requestobject_8',['RequestObject',['../classcom_1_1teragrep_1_1cfe18_1_1requestfilter_1_1RequestObject.html#ad916dba74a3aef846c01058a02382763',1,'com::teragrep::cfe18::requestfilter::RequestObject']]],
-  ['result_9',['Result',['../classcom_1_1teragrep_1_1cfe18_1_1requestfilter_1_1Result.html#a8c6fddaea68bcc128dcd31b172c83805',1,'com::teragrep::cfe18::requestfilter::Result']]],
-  ['retrievecapturestorages_10',['retrieveCaptureStorages',['../interfacecom_1_1teragrep_1_1cfe18_1_1StorageMapper.html#a58ec95cc7c3aceee315514b167ca0d11',1,'com::teragrep::cfe18::StorageMapper']]],
-  ['retrieveflowstorages_11',['retrieveFlowStorages',['../interfacecom_1_1teragrep_1_1cfe18_1_1StorageMapper.html#a41a371a8dd6967c359595b5d36152ae5',1,'com::teragrep::cfe18::StorageMapper']]]
+  ['removecapturestorage_1',['removeCaptureStorage',['../classcom_1_1teragrep_1_1cfe18_1_1handlers_1_1StorageController.html#a16c5cc858c7f76e8745a46484635e4a9',1,'com::teragrep::cfe18::handlers::StorageController']]],
+  ['removeflowstorage_2',['removeFlowStorage',['../classcom_1_1teragrep_1_1cfe18_1_1handlers_1_1StorageController.html#a08a54211dea97ca9979ee51302f597ce',1,'com::teragrep::cfe18::handlers::StorageController']]],
+  ['removehost_3',['removeHost',['../classcom_1_1teragrep_1_1cfe18_1_1handlers_1_1HostGroupController.html#a6c781998a5ad4fcc7afe4fbe40006999',1,'com::teragrep::cfe18::handlers::HostGroupController']]],
+  ['removehub_4',['removeHub',['../classcom_1_1teragrep_1_1cfe18_1_1handlers_1_1HubController.html#ad7c8f108be903c0874b6b6b16c85ca8d',1,'com::teragrep::cfe18::handlers::HubController']]],
+  ['removelinkage_5',['removeLinkage',['../classcom_1_1teragrep_1_1cfe18_1_1handlers_1_1LinkageController.html#aa043ab13e50d1e08c1f0eef11f404c82',1,'com::teragrep::cfe18::handlers::LinkageController']]],
+  ['requestidentity_6',['requestidentity',['../classcom_1_1teragrep_1_1cfe18_1_1requestfilter_1_1RequestIdentity.html#ac9dbf431f090b215ae3db0ee9f00b027',1,'com.teragrep.cfe18.requestfilter.RequestIdentity.RequestIdentity()'],['../classcom_1_1teragrep_1_1cfe18_1_1requestfilter_1_1RequestIdentity.html#a0cba0e4d72a6e7fcd1bf02f58d7d15ba',1,'com.teragrep.cfe18.requestfilter.RequestIdentity.RequestIdentity(final UUID uuid)']]],
+  ['requestobject_7',['RequestObject',['../classcom_1_1teragrep_1_1cfe18_1_1requestfilter_1_1RequestObject.html#ad916dba74a3aef846c01058a02382763',1,'com::teragrep::cfe18::requestfilter::RequestObject']]],
+  ['result_8',['Result',['../classcom_1_1teragrep_1_1cfe18_1_1requestfilter_1_1Result.html#a8c6fddaea68bcc128dcd31b172c83805',1,'com::teragrep::cfe18::requestfilter::Result']]],
+  ['retrievecapturestorages_9',['retrieveCaptureStorages',['../interfacecom_1_1teragrep_1_1cfe18_1_1StorageMapper.html#a58ec95cc7c3aceee315514b167ca0d11',1,'com::teragrep::cfe18::StorageMapper']]],
+  ['retrieveflowstorages_10',['retrieveFlowStorages',['../interfacecom_1_1teragrep_1_1cfe18_1_1StorageMapper.html#a41a371a8dd6967c359595b5d36152ae5',1,'com::teragrep::cfe18::StorageMapper']]]
 ];
