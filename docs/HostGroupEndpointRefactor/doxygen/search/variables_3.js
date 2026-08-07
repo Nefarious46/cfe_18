46cd4a88c8ab270d3d48dbe343b48b89dfa64c65
Nefarious46 <146811860+Nefarious46@users.noreply.github.com>
2026-08-07 10:47:36 +0000

    Deploying to gh-pages from @ Nefarious46/cfe_18@c37d015f631d82b7070b2858842ecfd97f4ef5c1 🚀

--- docs/HostGroupEndpointRefactor/doxygen/search/variables_3.js
+++ docs/HostGroupEndpointRefactor/doxygen/search/variables_3.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['errorcode_0',['errorCode',['../enumcom_1_1teragrep_1_1cfe18_1_1MariaDBError.html#a89ab1465e2319fa9c94ce20ab09b0a0b',1,'com::teragrep::cfe18::MariaDBError']]]
+  ['errorcode_0',['errorCode',['../enumcom_1_1teragrep_1_1cfe18_1_1MariaDBError.html#a89ab1465e2319fa9c94ce20ab09b0a0b',1,'com::teragrep::cfe18::MariaDBError']]],
+  ['existingrecordtypeconflict_1',['EXISTINGRECORDTYPECONFLICT',['../enumcom_1_1teragrep_1_1cfe18_1_1MariaDBError.html#a8af39a3331e141ae4f5b294f6583c229',1,'com::teragrep::cfe18::MariaDBError']]]
 ];
